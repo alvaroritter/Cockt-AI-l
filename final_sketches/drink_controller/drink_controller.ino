e90e936856b6cb76c9c067d1fe52e1a5c80cafd9
Daniel San Jose Pro <sanjose.daniel@gmail.com>
2021-01-17 11:33:08 +0100

    First upload of final sketches

---
+++ final_sketches/drink_controller/drink_controller.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>;
+
+Servo servo1; 
+Servo servo2;
+
+int carReady = 1; // ready, busy : 0
+
+int pos1 = 90, pos2 = 90, dir = 0, motor = 1, button = 3, xValue, yValue; // Variables for each motor position and motor selection, button for test purposes
+int pinMotor1 = 5;
+int pinMotor2 = 6;
+int openAngle = 150;
+int closedAngle = 10;
+
+void setup() {
+  Serial.begin(9600);
+  servo1.attach(pinMotor1);
+  servo2.attach(pinMotor2);
+  pinMode(button, INPUT);
+}
+
+
+void serve_bottle(Servo servo, int amount){
+  servo.write(openAngle); // Open position
+  delay(amount);
+  servo.write(closedAngle); // Closed position
+}
+
+
+void loop() {
+  int startDrink = digitalRead(button);
+  if (startDrink && carReady){
+    carReady = 0;
+    serve_drink();
+    change_drink(1, 2);
+    while(!carReady){
+      if(Serial.available() > 0){
+        carReady = Serial.read();
+      }
+      delay(10);
+    }
+    delay(1000); // wait 1 sec, no need to rush
+    serve_drink();
+    change_drink(2, 3);
+  }
+}
+
+void change_drink(int startDrink, int destinationDrink){
+  int change = destinationDrink - startDrink;
+  switch(change){
+    case 0:
+      break;
+    case 1:
+      motor = 2; // Switches to other motor for next serving
+      Serial.write('f');
+      break;
+    case -1: 
+      motor = 1; // Switches to other motor for next serving
+      Serial.write('b');
+  }
+}
+
+void serve_drink(){
+   switch(motor){
+     case 1:
+       serve_bottle(servo1, 5000);
+       break;
+
+     case 2: 
+       serve_bottle(servo2, 5000);
+   }
+}
---
+++ final_sketches/platform_slave/platform_slave.ino
@@ -0,0 +1,61 @@
+int lEnable = 10;
+int lForward = 9;
+int lBackward = 8;
+int rEnable = 11;
+int rForward = 13;
+int rBackward = 12;
+
+int pulseWidth = 200;
+char mode;
+
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode (rBackward, OUTPUT);
+  pinMode (rForward, OUTPUT);
+  pinMode (rEnable, OUTPUT);
+  pinMode (lEnable, OUTPUT);
+  pinMode (lForward, OUTPUT);
+  pinMode (lBackward, OUTPUT);
+}
+
+void loop() {
+  if( Serial.available() > 0 ){
+    mode = Serial.read();
+    if( mode == 'f' ){
+      move_forward();
+    }
+    if( mode == 'b' ){
+      move_backward();
+    }
+  }
+}
+
+void move_forward() {
+  int timeMoving = 1000;
+  digitalWrite (rForward, HIGH);
+  digitalWrite (rBackward, LOW);
+  digitalWrite (lForward, HIGH);
+  digitalWrite (lBackward, LOW);
+  analogWrite (rEnable, pulseWidth);
+  analogWrite (lEnable, pulseWidth);
+  delay (timeMoving);
+  analogWrite (rEnable, 0);
+  analogWrite (lEnable, 0);
+  Serial.write(1); // ready to get new instructions
+}
+
+void move_backward() {
+  int timeMoving = 1000;
+  digitalWrite (rBackward, HIGH);
+  digitalWrite (rForward, LOW);
+  digitalWrite (lBackward, HIGH);
+  digitalWrite (lForward, LOW);
+  analogWrite (rEnable, pulseWidth);
+  analogWrite (lEnable, pulseWidth);
+  delay (timeMoving);
+  analogWrite (rEnable, 0);
+  analogWrite (lEnable, 0);
+  Serial.write(1); // ready to get new instructions
+}
